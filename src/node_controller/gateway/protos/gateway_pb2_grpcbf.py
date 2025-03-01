a6926ef594333d126f34b30be4db8804db0456c4
0xf965 <thisemaildoesnotexists@anon.erg>
2025-03-01 23:37:35 +0100

    Any.Metadata -> Metadata

--- src/node_controller/gateway/protos/gateway_pb2_grpcbf.py
+++ src/node_controller/gateway/protos/gateway_pb2_grpcbf.py
@@ -4,8 +4,8 @@ StartService_input_indices = {
     1: gateway_pb2.Client,
     2: gateway_pb2.RecursionGuard,
     3: gateway_pb2.Configuration,
-    4: gateway_pb2.celaut__pb2.Any.Metadata.HashTag.Hash,
-    5: gateway_pb2.celaut__pb2.Any.Metadata,
+    4: gateway_pb2.celaut__pb2.Metadata.HashTag.Hash,
+    5: gateway_pb2.celaut__pb2.Metadata,
     6: gateway_pb2.celaut__pb2.Service,
 }
 StartService_input_message_mode = {1: True, 2: True, 3: True, 4: True, 5: True, 6: False}  # False yield a Dir.
