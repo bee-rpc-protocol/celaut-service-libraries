d88dd6cdc0cfc7cbb6a5636cbdae90064a1e4f32
0xf965 <thisemaildoesnotexists@anon.erg>
2025-06-25 10:21:52 +0200

    Fix import statement and clean up unused PackOutput_indices in gateway_bee.py

--- src/node_controller/gateway/protos/gateway_bee.py
+++ src/node_controller/gateway/protos/gateway_bee.py
@@ -1,4 +1,4 @@
-from protos import celaut_pb2, pack_pb2
+from node_controller.gateway.protos import celaut_pb2
 
 StartService_input_indices = {
     1: celaut_pb2.Client,
@@ -9,10 +9,3 @@ StartService_input_indices = {
     6: celaut_pb2.Service,
 }
 StartService_input_message_mode = {1: True, 2: True, 3: True, 4: True, 5: True, 6: False}  # False yield a Dir.
-
-PackOutput_indices = {
-    1: pack_pb2.PackOutputServiceId,
-    2: celaut_pb2.Metadata,
-    3: pack_pb2.Service,
-    4: pack_pb2.PackOutputError
-}
